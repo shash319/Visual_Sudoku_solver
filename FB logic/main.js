8b092486cf84682f1ac2b9275292329dababaa6f
Shasawat Pal <shashwatpal2001@gmail.com>
2024-01-03 00:46:07 +0530

    my new changes

---
+++ FB logic/main.js
@@ -0,0 +1,32 @@
+// btn.addEventListner("click", (e) => {
+//     e.preventDefault();
+//     console.log("click", document.querySelectorAll('.item'));
+//     document.querySelector("#my-form").style.background = "blue";
+// })
+
+// const nameInput= document.querySelector("#name");
+// nameInput.addEventListener('input', e => {
+//     console.log(nameInput.value);
+//     document.querySelector('container').append(nameInput.value + "=>");
+// })
+
+// function itemTrigger() {
+//     console.log("item clicked");
+// }
+
+
+// //------------------------------------------------------------------------
+// const nameInput = document.querySelector("#name");
+// const emailInput = document.querySelector("#email");
+// const submitBtn = document.querySelector(".btn");
+// const first = document.querySelector("items").children[0];
+// const second= document.querySelector(".items").children[1];
+
+// submitBtn.addEventListener("click", function(e){
+//     e.preventDefault();
+//     const name = nameInput.value;
+//     const email = emailInput.value;
+//     first.innerHTML= "<h1> $ {name} </h1>"
+//     second.innerHTML = "<h1> ${email}</h1>"
+// })
+
